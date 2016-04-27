8f9818a082341be96a93cc800fe2bca2ff829057
Venu Nalla <veenunalla@gmail.com>
2016-04-27 12:11:51 +0530

    Updated the Class and Create SuperClass for the Activity.

---
+++ app/src/main/java/com/svs/hztb/Activities/AbstractActivity.java
@@ -0,0 +1,60 @@
+package com.svs.hztb.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+
+import com.svs.hztb.Utils.LoadingBar;
+
+
+/**
+ * Created by bharathmaheshwaram on 22/04/16.
+ */
+public abstract class AbstractActivity extends AppCompatActivity {
+
+    protected LoadingBar _loader;
+    protected String BASE_URL = "http://singletomingle.esy.es/registrations.php?";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        _loader=new LoadingBar(this);
+    }
+
+
+    protected <T extends View> T getView(int id) {
+        return (T) findViewById(id);
+    }
+
+
+    protected void displayMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void pushActivity(Class className){
+        Intent startActivity = new Intent(this,className);
+        startActivity(startActivity);
+    }
+
+    public void showLoader(){
+        if(_loader!=null && !_loader.isShowing()){
+            _loader.show();
+        }
+    }
+
+    public void cancelLoader(){
+        if(_loader!=null && _loader.isShowing()){
+            _loader.cancel();
+        }
+    }
+
+
+    protected boolean isEmailValid(String emailId){
+        return Patterns.EMAIL_ADDRESS.matcher(emailId).matches();
+    }
+
+}
--- app/src/main/java/com/svs/hztb/Activities/HZTBActivity.java
+++ app/src/main/java/com/svs/hztb/Activities/HZTBActivity.java
@@ -1,15 +1,17 @@
-package com.svs.hztb;
+package com.svs.hztb.Activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class HZTBActivity extends AppCompatActivity {
+import com.svs.hztb.R;
+
+public class HZTBActivity extends AbstractActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
