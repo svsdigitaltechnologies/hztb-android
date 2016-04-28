a4da80a491339c5cb377ff7c1ce20b8aad0dfc21
Venu Nalla <veenunalla@gmail.com>
2016-04-28 08:34:57 +0530

    Added HomeActivity, Splash Screen, Mobile Phone registration screen. Completed MobilePhoneRegistration Screen UI.

---
+++ app/src/main/java/com/svs/hztb/Activities/SplashScreenActivity.java
@@ -0,0 +1,55 @@
+package com.svs.hztb.Activities;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.svs.hztb.R;
+
+
+
+public class SplashScreenActivity extends AbstractActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+         /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                pushActivity(MobilePhoneRegistrationActivity.class);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
