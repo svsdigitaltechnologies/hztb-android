f8407b34a8ea4604768fd915886b9ab52f96e4e2
Venu Nalla <veenunalla@gmail.com>
2016-05-17 23:59:39 +0530

    Created Bean Classes for Group and Contact objects

---
+++ app/src/main/java/com/svs/hztb/Bean/Group.java
@@ -0,0 +1,13 @@
+package com.svs.hztb.Bean;
+
+import java.util.ArrayList;
+
+/**
+ * Created by VenuNalla on 5/17/16.
+ */
+public class Group  {
+
+    private Contact contact;
+    boolean isSelect;
+
+}
