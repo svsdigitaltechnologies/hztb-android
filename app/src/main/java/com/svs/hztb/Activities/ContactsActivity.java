d48b35ae7ddf32b047af862085ecf2a4f4b19fb4
Venu Nalla <veenunalla@gmail.com>
2016-05-18 08:39:43 +0530

    Created Contacts Activity

---
+++ app/src/main/java/com/svs/hztb/Activities/ContactsActivity.java
@@ -0,0 +1,16 @@
+package com.svs.hztb.Activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.svs.hztb.R;
+
+public class ContactsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+    }
+
+}
